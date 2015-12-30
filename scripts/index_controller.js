6070b20c9dce66b5343f0316b432998458f42c4d
csrisakvarakul <c.srisakvarakul@gmail.com>
2015-12-30 11:23:24 +0000

    created function for Check Answer button and associated console message

--- scripts/index_controller.js
+++ scripts/index_controller.js
@@ -28,6 +28,11 @@ function viewMethod(systemModel, outputModel, experimentStatus, chartConfig, tab
 	view.isDisabled_background = false;
 	view.isDisabled_run = false;
 	view.isDisabled_wash = true;
+	view.isDisabled_check = true;
+	view.Kd_correct = false;
+	view.kOn_correct = false;
+	view.kOff_correct = false;
+	view.all_Correct = false;
 		// stored cookies data
 	view.storedDataPrompt = false;
 	view.cookiesData = {
@@ -61,7 +66,7 @@ function viewMethod(systemModel, outputModel, experimentStatus, chartConfig, tab
 		view.storedDataPrompt = false;
 	};
 
-/* g) creating function for set "zero" button */
+/* c) creating function for set "zero" button */
 	view.set_background = function() {
 		view.backgroundSet = angular.copy(view.output.RU_On_Output[view.output.RU_On_Output.length-1]);
 		view.backgroundUnitsSet = angular.copy(view.output.unitAdjust);
@@ -70,7 +75,7 @@ function viewMethod(systemModel, outputModel, experimentStatus, chartConfig, tab
 		view.output.minimum_fLC_input = 1;
 	};
 
-/* h) creating function for "run experiment" button  */
+/* d) creating function for "run experiment" button  */
 	view.runExperiment = function (new_magnitudeSelected, new_fLC, new_timeOn) {
 		view.output.magnitudeAdjust = view.output.magnitudePool[new_magnitudeSelected];
 		view.output.unitAdjust = view.output.unitPool[new_magnitudeSelected];
@@ -84,10 +89,11 @@ function viewMethod(systemModel, outputModel, experimentStatus, chartConfig, tab
 		view.experiment.timeOfDayCounter();
 		view.isDisabled_run = true;
 		view.isDisabled_wash = false;
+		view.isDisabled_check = true;
 		view.cookies.putObject("storedData", view.cookiesData);
 	};
 
-/* i) creating function for "wash-up" button */
+/* e) creating function for "wash-up" button */
 	view.washUp = function(new_timeOn) {
 		view.output.plotCoordinatesOff(view.output.currentStep, view.output.totalSteps, new_timeOn, view.system.kOff, view.system.RU0, view.backgroundSet);
 		view.output.plotCompileLabelOff();
@@ -95,22 +101,25 @@ function viewMethod(systemModel, outputModel, experimentStatus, chartConfig, tab
 		view.isDisabled_run = false;
 		view.isDisabled_wash = true;
 		view.cookies.putObject("storedData", view.cookiesData);
+		view.isDisabled_check = true;
 	}; 
 
-/* k) creating function for "home" button */
-	view.goHome = function () {
+/* f) creating function for "home" button */
+	view.goHome = function() {
 		view.experiment.endOfExperimentTime = angular.copy(view.experiment.timeOfDay);
 		view.experiment.timeOfDay = view.experiment.startOfDay;
 		view.experiment.dayOfExperimentCounter();
+		view.isDisabled_check = true;
 	};
 
-/* l) creating function for "clear graph" button */
+/* g) creating function for "clear graph" button */
 	view.clearChart = function() {
 		view.output.RU_CompiledLabelPlotAll.length = 0;
+		view.isDisabled_check = true;
 	};
 
-/* m) creating a function for "restart" button */
-	view.restart = function () {
+/* h) creating a function for "restart" button */
+	view.restart = function() {
 		view.experiment.daysLeft = view.experiment.daysAllowed;
 		view.experiment.timeOfDay = view.experiment.startOfDay;
 		view.experiment.steps = 0;
@@ -124,9 +133,41 @@ function viewMethod(systemModel, outputModel, experimentStatus, chartConfig, tab
 		view.output.RU_CompiledLabelPlotAll.length = 0;
 		view.table.data.length = 0;
 		view.restartCounter++;
+		view.isDisabled_run = false;
+		view.isDisabled_wash = true;
+		view.isDisabled_check = true;
 	};
 
-/* n) initialise application to generate unique values for the new system, else load previous experiment state from cookies */
+/* i) creating a function for "check" answer button */
+	view.check = function(check_Kd, check_kOn, check_kOff) {
+		view.isDisabled_check = false;
+			// check if Kd answer is within acceptable range
+		if (check_Kd > 0.99*view.system.Kd && check_Kd < 1.01*view.system.Kd) {
+			view.Kd_correct = true;
+		} else {
+			view.Kd_correct = false;
+		}
+			// check if kOn answer is within acceptable range
+		if (check_kOn > 0.99*view.system.kOn && check_kOn < 1.01*view.system.kOn) {
+			view.kOn_correct = true;
+		} else {
+			view.kOn_correct = false;
+		}
+			// check if kOff answer is within acceptable range
+		if (check_kOff > 0.99*view.system.kOff && check_kOff < 1.01*view.system.kOff) {
+			view.kOff_correct = true;
+		} else {
+			view.kOff_correct = false;
+		}
+			// if all answer correct, trigger action
+		if (view.Kd_correct === true && view.kOn_correct === true && view.kOff_correct === true) {
+			view.all_Correct = true;
+		} else {
+			view.all_Correct = false;
+		}
+	};
+
+/* j) initialise application to generate unique values for the new system, else load previous experiment state from cookies */
 
 	if (view.cookies.getObject("storedData") === undefined) { // if there are no data, generate a new system
 		view.system.loadNewPair(view.vol, view.RPUM);
