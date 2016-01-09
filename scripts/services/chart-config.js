fc1753ce5d03127aeb16f0b083473be403d50b1a
csrisakvarakul <c.srisakvarakul@gmail.com>
2016-01-09 12:05:55 +0000

    started on zoom in graph and also pop up to confirm restart

--- scripts/services/chart-config.js
+++ scripts/services/chart-config.js
@@ -43,6 +43,27 @@ function chartCreate(outputModel) {
 		tooltip: {
 			show: true,
 			content: "(%x, %y)"
+		},
+		selection: {
+			mode: "xy"
 		}
 	};
+/*
+	$("#flot").bind("plotselected", function (event, ranges) {
+			/ clamp the zooming to prevent eternal zoom
+		if (ranges.xaxis.to - ranges.xaxis.from < 0.00001) {
+			ranges.xaxis.to = ranges.xaxis.from + 0.00001;
+		}
+		if (ranges.yaxis.to - ranges.yaxis.from < 0.00001) {
+			ranges.yaxis.to = ranges.yaxis.from + 0.00001;
+		}
+			// do the zooming
+		plotObj = $.plot("#flot", chart.dataset,
+			$.extend(true, {}, options, {
+				xaxis: { min: ranges.xaxis.from, max: ranges.xaxis.to },
+				yaxis: { min: ranges.yaxis.from, max: ranges.yaxis.to }
+			})
+		);
+	});
+*/
 }
