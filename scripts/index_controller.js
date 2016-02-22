61cf86c5c4d1cf6fba04efaea1b2e0db0d551a9b
csrisakvarakul <c.srisakvarakul@gmail.com>
2016-02-22 11:50:26 +0000

    final version for Part II project 2016. Fixed equilibrium reached messages etc.

--- scripts/index_controller.js
+++ scripts/index_controller.js
@@ -158,7 +158,6 @@ function viewMethod(systemModel, outputModel, experimentStatus, chartConfig, tab
 		view.backgroundSet = 0;
 		view.backgroundUnitsSet = null;
 		view.isDisabled_background = false;
-		view.experiment.EqTimeReachedOnce = false;
 		view.output.minimum_fLC_input = 0;
 		view.experiment.comboStreak = 0;
 		view.experiment.isDisabled_comboOne = false;
@@ -168,6 +167,7 @@ function viewMethod(systemModel, outputModel, experimentStatus, chartConfig, tab
 		view.experiment.isDisabled_comboFive = false;
 		view.experiment.isDisabled_comboSix = false;
 		view.experiment.isDisabled_comboSeven = false;
+		view.EqTimeReachedOnce = false;
 			// remove all data in existing arrays
 		view.output.fLC.length = 0;
 		view.output.timeOn.length = 0;
@@ -292,7 +292,6 @@ function viewMethod(systemModel, outputModel, experimentStatus, chartConfig, tab
 		view.backgroundSet = 0;
 		view.backgroundUnitsSet = null;
 		view.isDisabled_background = false;
-		view.experiment.EqTimeReachedOnce = false;
 		view.output.minimum_fLC_input = 0;
 		view.experiment.comboStreak = 0;
 		view.experiment.isDisabled_comboOne = false;
@@ -302,6 +301,7 @@ function viewMethod(systemModel, outputModel, experimentStatus, chartConfig, tab
 		view.experiment.isDisabled_comboFive = false;
 		view.experiment.isDisabled_comboSix = false;
 		view.experiment.isDisabled_comboSeven = false;
+		view.EqTimeReachedOnce = false;
 			// remove all data in existing arrays
 		view.output.fLC.length = 0;
 		view.output.timeOn.length = 0;
@@ -343,6 +343,7 @@ function viewMethod(systemModel, outputModel, experimentStatus, chartConfig, tab
 			stored_comboFiveDisabled: view.experiment.isDisabled_comboFive,
 			stored_comboSixDisabled: view.experiment.isDisabled_comboSix,
 			stored_comboSevenDisabled: view.experiment.isDisabled_comboSeven,
+			stored_eqTimeReachedOnce: view.EqTimeReachedOnce,
 				// stored output
 			store_tableData: view.table.data,
 			store_fLC: view.output.fLC,
@@ -390,6 +391,7 @@ function viewMethod(systemModel, outputModel, experimentStatus, chartConfig, tab
 		view.experiment.isDisabled_comboFive = view.cookiesData.stored_comboFiveDisabled;
 		view.experiment.isDisabled_comboSix = view.cookiesData.stored_comboSixDisabled;
 		view.experiment.isDisabled_comboSeven = view.cookiesData.stored_comboSevenDisabled;
+		view.EqTimeReachedOnce = view.cookiesData.stored_eqTimeReachedOnce;
 			// set output to as stored
 		for (var i = 0; i < view.cookiesData.store_tableData.length; i++) {
 			view.table.data.push(view.cookiesData.store_tableData[i]);
